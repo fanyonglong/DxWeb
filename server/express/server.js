d763491b7c7da9830fcc2c56b390d2f505208fec
fanyonglong <fyl@qq.com>
2017-12-26 20:20:34 +0800

    ff

--- server/express/server.js
+++ server/express/server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var http=require('http');
@@ -16,23 +17,63 @@ var rootPath=path.resolve(__dirname,'../../www/express-site');
  * 
 */
 
-//app.use(express.static('assets'));
-//app.use('/assets', express.static(path.join(rootPath,'assets')));
+
+app.use('/assets', express.static(path.join(rootPath,'assets')));
 // app.get('/assets/*', function(req, res) {
 //   res.sendFile(req.url,{
 //     root:rootPath
 //   });
 // });
-app.use(function(req,res,next){
-  if(req.url.indexOf('/assets')==0)
-  {
-    res.sendFile(req.url,{
-      root:rootPath
+
+// 中间件拦截
+// app.use(function(req,res,next){
+
+//   if(req.url.indexOf('/assets')==0)
+//   {
+//     res.sendFile(req.url,{
+//       root:rootPath
+//     });
+//   }else{
+//     next();
+//   }
+ 
+// })
+
+
+
+app.all('/getList',function(req,res){
+  console.log(req.query.timeout);
+if(req.query.timeout&&req.query.timeout>0)
+{
+  setTimeout(function(){
+    res.json({
+      retStatus:200,
+      data:['aaa','bbbb']
     });
-   // res.end();
-  }
-  next();
-})
+  },Number(req.query.timeout));
+  }else{
+    res.json({
+      retStatus:200,
+      data:['aaa','bbbb']
+    });
+}
+});
+app.all('/getList2',function(req,res,next){
+
+  //res.statusMessage='请求的接口不正确';
+ // res.status(400).end();
+
+ // res.statusMessage="send fail";
+ //res.statusCode=500;
+ res.status(500).send('请求的接口不正确');
+
+  //res.send(500,{ error: 'something blew up' });
+  // res.json({
+  //   retStatus:200,
+  //   data:['aaa','bbbb']
+  // });
+
+});
 
 
 app.get('/', function(req, res) {
@@ -40,6 +81,19 @@ app.get('/', function(req, res) {
     root:rootPath
   });
 });
+
+// 错误中间件
+app.use(function(err, req, res, next){
+  if(err){
+    var code = err.code;
+      var message = err.message;
+      res.writeHead(code, message, {'content-type' : 'text/plain'});
+      res.end(message);
+  }else{
+    next();
+  }
+});
+
 
 // npm run web -- --open
 app.listen(5888,()=>{
