7d94a471693bd41b033bf6ce43af9e71e9e559df
fanyonglong <315144604@qq.com>
2018-07-09 15:48:28 +0800

    aaa

--- server/node/server.js
+++ server/node/server.js
@@ -11,9 +11,20 @@ const mime=require('mime');
 const opn=require('opn');
 const child_process=require('child_process');
 console.log(args.open);
+/*
+创建一个Server服务应用层
+**/
 var App=(function(){
     var routers=[];
+    /**
+     * 创建http请求路由处理类
+    */
     class Router{
+        /**
+         * 
+         * @param {string} path  匹配路径
+         * @param {function} handle  处理函数
+         */
         constructor(path,handle)
         {
               this.path=pathToRegexp(path,this.keys=[]);
@@ -41,16 +52,23 @@ var App=(function(){
         }
         
     }
+    /**
+     * 
+     * @param {httpRequest} req  请求对象 
+     * @param {httpResponse} res 响应对象
+     */
     function App(req,res)
     {   
+        //监听服务请求
         App.handle(req,res);
     }
     App._events={};
+    // 继承node 事件机制
     Object.assign(App,EventEmitter.prototype);
     App.handle=function(req,res)
     {
         let path=req.url;
-        let routerlist= routers.filter(item=>item.exec(path));
+        let routerlist= routers.filter(item=>item.exec(path));// 根据请求路径查找对应处理路由
         let index=0;
         function next(error)
         {
@@ -69,25 +87,40 @@ var App=(function(){
     App.init=function()
     {
         App.use(function(req,res){
-            res.req=req;
-            req.res=res;
-            Reflect.setPrototypeOf(req,App.request);
-            Reflect.setPrototypeOf(res,App.response);
+            res.req=req;// 原始request
+            req.res=res;// 原始response
+            Reflect.setPrototypeOf(req,App.request);// 继承自定义request
+            Reflect.setPrototypeOf(res,App.response);// 继承自定义response
         });
     }
+    /**
+     * 创建代理层
+     * @param {function} handler  处理函数
+     */
     App.use=handler=>{
-        let router=new Router('(.*)',handler);
+        let router=new Router('(.*)',handler);// 所有路径都处理
         router.use=true;
-        let index=routers.slice().reverse().findIndex(item=>item.use===true);
+        let index=routers.slice().reverse().findIndex(item=>item.use===true);// 插入到其它的use后面
         routers.splice(index+1,0,router);
     };
+    /**
+     * 创建一个路由
+     * @param {string} path  路径
+     * @param {function} handler 
+     */
     App.router=(path,handler)=>
     {
         routers.push(new Router(path,handler));
     }
+    /**
+     * 扩展http 原始request 对象
+    */
     App.request=Object.assign(Object.create(http.IncomingMessage.prototype),{
 
     });
+      /**
+     * 扩展http 原始response 对象
+    */
     App.response=Object.assign(Object.create(http.ServerResponse.prototype),{
         sendFile(filePath)
         {
@@ -143,11 +176,11 @@ var App=(function(){
     App.init(); 
     return App;
 }());
-var server=http.createServer(App);
 
 App.use((()=>{
     let static=pathToRegexp('/assets/(.*)')
     return function(req,res,next){
+        // 如果请求路径是静态资源，直接拦载，响应输出
         if(static.test(req.url))
         {
             res.sendFile(path.resolve(__dirname,'../../www/express-site/'+req.url))
@@ -163,13 +196,15 @@ App.router('/list',function(req,res){
     res.json({list:[1,2,3,4]});
 });
 
+// 创建一个http服务器
+var server=http.createServer(App);
 
 
 server.on('close',function(){
 
     console.log('close server');
 });
-
+// 开始启动监听
 server.listen(5888,function(){
     console.log('服务已启动');
  
